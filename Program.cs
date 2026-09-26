89bc3bd3af8b42d0d567339c736d533a4842dcfc
SaqlainCSE <saqlaincse@gmail.com>
2026-09-26 14:26:36 +0600

    data annotations updated

--- Program.cs
+++ Program.cs
@@ -19,11 +19,11 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
                                 Errors = e.Value != null ? e.Value.Errors.Select(x => x.ErrorMessage).ToArray() : new string[0]
                             }).ToList();
 
-        var errorString = string.Join("; ", errors.Select(e => $"{e.Field}: {string.Join(", ", e.Errors)}"));
+        // var errorString = string.Join("; ", errors.Select(e => $"{e.Field}: {string.Join(", ", e.Errors)}"));
         return new BadRequestObjectResult(new
         {
             Message = "Validation Failed",
-            Errors = errorString
+            Errors = errors
         });
     };
 });
